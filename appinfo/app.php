0c3f27a7877a2b7f9c2f429174b16d77b0d4632b
icem0@online.de <icem0@online.de>
2025-04-28 20:27:57 +0200

    .

--- appinfo/app.php
+++ appinfo/app.php
@@ -3,22 +3,9 @@
 namespace OCA\Stempeluhr\AppInfo;
 
 use OCP\AppFramework\App;
-use OCP\Util;
-use OCP\Security\CSP\ContentSecurityPolicy;
-use OCP\Security\CSP\ContentSecurityPolicyManager;
 
-$app = new App('stempeluhr');
-$container = $app->getContainer();
-
-/** @var ContentSecurityPolicyManager $cspManager */
-$cspManager = $container->query(ContentSecurityPolicyManager::class);
-$policy = new ContentSecurityPolicy();
-
-// Beispiel: Erlaube Skripte nur von der eigenen Domain
-$policy->addAllowedScriptDomain('self');
-
-// Weitere Richtlinien können hier hinzugefügt werden
-// $policy->addAllowedStyleDomain('self');
-// $policy->addAllowedImageDomain('self');
-
-$cspManager->addDefaultPolicy($policy);
+class Application extends App {
+    public function __construct(array $urlParams = []) {
+        parent::__construct('stempeluhr', $urlParams);
+    }
+}
