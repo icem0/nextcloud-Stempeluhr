f99914953edcb910c33cd80776748df15d03ae89
icem0@online.de <icem0@online.de>
2025-04-28 19:59:17 +0200

    .

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -1,6 +1,17 @@
 <?php
-return [
-    'routes' => [
-        ['name' => 'page#index', 'url' => '/', 'verb' => 'GET'],
+
+namespace OCA\Stempeluhr\AppInfo;
+
+use OCP\AppFramework\App;
+
+$app = new App('stempeluhr');
+$application = $app->getContainer()->query('OCA\Stempeluhr\Controller\PageController');
+
+$app->getContainer()->getServer()->getRouter()->addRoute([
+    'name' => 'page#index',
+    'url' => '/',
+    'verb' => 'GET',
+    'defaults' => [
+        '_controller' => 'OCA\Stempeluhr\Controller\PageController::index'
     ]
-];
+]);
