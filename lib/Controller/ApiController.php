562b392b3ff394b010e9b39f0b682a8fc6a6786c
icem0@online.de <icem0@online.de>
2025-04-28 19:07:35 +0200

    ..

--- lib/Controller/ApiController.php
+++ lib/Controller/ApiController.php
@@ -1,31 +1,20 @@
 <?php
-
-declare(strict_types=1);
-
 namespace OCA\Stempeluhr\Controller;
 
-use OCP\AppFramework\Http;
-use OCP\AppFramework\Http\Attribute\ApiRoute;
-use OCP\AppFramework\Http\Attribute\NoAdminRequired;
-use OCP\AppFramework\Http\DataResponse;
-use OCP\AppFramework\OCSController;
+use OCP\AppFramework\Controller;
+use OCP\IRequest;
+use OCP\AppFramework\Http\TemplateResponse;
 
-/**
- * @psalm-suppress UnusedClass
- */
-class ApiController extends OCSController {
-	/**
-	 * An example API endpoint
-	 *
-	 * @return DataResponse<Http::STATUS_OK, array{message: string}, array{}>
-	 *
-	 * 200: Data returned
-	 */
-	#[NoAdminRequired]
-	#[ApiRoute(verb: 'GET', url: '/api')]
-	public function index(): DataResponse {
-		return new DataResponse(
-			['message' => 'Hello world!']
-		);
-	}
+class PageController extends Controller {
+    public function __construct(string $AppName, IRequest $request) {
+        parent::__construct($AppName, $request);
+    }
+
+    /**
+     * @NoAdminRequired
+     * @NoCSRFRequired
+     */
+    public function index(): TemplateResponse {
+        return new TemplateResponse('stempeluhr', 'main');
+    }
 }
---
+++ templates/main.php
@@ -0,0 +1,4 @@
+<div id="app">
+    <h1>Willkommen bei der Stempeluhr!</h1>
+    <p>Hier kannst du deine Arbeitszeiten stempeln.</p>
+</div>
